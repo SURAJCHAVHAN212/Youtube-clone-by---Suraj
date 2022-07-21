983b7328b9adce8929672229cc8084bd3480bd42
Suraj <97757309+SURAJCHAVHAN212@users.noreply.github.com>
2022-07-21 13:17:13 +0530

    Update index.js
    some changes

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-/*
+const menu = document.querySelector('#menu');
+console.log(menu);
 
-Need 2 Things:
-(1) Show/Display Videos on Homepage
-(2) Search When Something is typed in the search-box
+const sidebar = document.querySelector('.sidebar');
 
-
-display()
-- Show/Display Videos on Homepage
-- Based Upon Most Popular Videos
-- Should be Called Directly WITHOUT Any Event 
-(No Hover, No Click , No On Click)
-- On Page Load
-
-
-
-searchVideo()
-- Search When Something is typed in the search-box
-- Take the "String" typed in search-box and search on that string 
-
-
-*/
+menu.addEventListener('click', function () {
+  sidebar.classList.toggle('show-sidebar');
+});
 
 let div = document.getElementById("videodiv");
 
-
-// (1) Show/Display Videos on Homepage
-
-
-
 async function display(){
+
+    // q = Popular Videos --> Popular%20Videos (%20 -> Space)
+
+    let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=popular%20videos&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
+
+    let data = await res.json();
+    
+    for({id:{videoId}}of data.items)
+    {
+    let videodiv= document.createElement("iframe")
+    videodiv.setAttribute("class","mons")
+    videodiv.src = `https://www.youtube.com/embed/${videoId}`;
+    videodiv.allow = 'fullscreen'
+    div.append(videodiv)
+    }
 
 }
 
 display();
-
-
-
-
-// (2) Search When Something is typed in the search-box
-
 async function searchVideos(){
     document.getElementById("videodiv").innerHTML=""; // Empty 
 
-    let query = document.getElementById("video").value; // Search String/ Query Stri
-
-//search for videos
+    let query = document.getElementById("video").value; // Search String/ Query String
 let res = await fetch(`https://youtube.googleapis.com/youtube/v3/search?q=${query}&type=video&key=AIzaSyA00OxUbfdfVAjHn-9l48s0t-n_j7CBbNs&maxResults=25`);
 let data = await res.json();
 
